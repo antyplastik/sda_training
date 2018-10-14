0f207895c161839cd279bfcd3be6b92c43da3a8f
Kamil P <antyplastik@gmail.com>
2018-10-14 13:56:17 +0200

    zadanie 4 dziala

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -2,14 +2,32 @@ package main;
 
 import zad_1.Sum;
 import zad_2.SumOfMultiples;
+import zad_4.StringInvertion;
 
 public class Runner {
     public static void main(String[] args) {
 //        1.
-    zad1();
+//        zad1();
 //        2.
-    zad2();
-
+//        zad2();
+//        3.
+//        zad3();
+//        4.
+        zad4();
+//        5.
+//        zad5();
+//        6.
+//        7.
+//        8.
+//        9.
+//        10.
+//        11.
+//        12.
+//        13.
+//        14.
+//        15.
+//        16.
+//        17.
     }
 
     public static void zad1() {
@@ -26,7 +44,15 @@ public class Runner {
     }
 
     public static void zad3() {
-        
+
+    }
+
+    public static void zad4() {
+        StringInvertion stringInvertion = new StringInvertion();
+        System.out.println("Odp. zad4: "+stringInvertion.executeStringInvertion());
+    }
+
+    private static void zad5() {
     }
 
 }
---
+++ src/main/java/zad_4/StringInvertion.java
@@ -0,0 +1,30 @@
+package zad_4;
+
+public class StringInvertion {
+
+    String string_to_invert;
+
+    public StringInvertion() {
+        System.out.println("===== Zad.4 =====");
+        this.string_to_invert = "programuje w javie";
+    }
+
+    public StringInvertion(String string_to_invert) {
+        System.out.println("===== Zad.4 =====");
+        this.string_to_invert = string_to_invert;
+    }
+
+    public String executeStringInvertion(){
+        string_to_invert = this.string_to_invert;
+        String strTmp = "";
+        int strLen = string_to_invert.length();
+
+
+        for (int i = 0; i < strLen; i++){
+          strTmp = strTmp + string_to_invert.charAt(strLen-1 - i);
+        }
+
+        return strTmp;
+    }
+
+}
